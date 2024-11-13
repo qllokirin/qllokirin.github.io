56d156ae8e39618b8f82642a9f65bb3de828a51b
qllokirin <2456590695@qq.com>
2024-11-13 13:55:26 +0800

    Site updated: 2024-11-13 13:55:24

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731408429730";
+const VERSION = "1731477309990";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
